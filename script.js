2e7707f3b3d49245f137747f5bd88df74ce0a266
Zaharia Vlad <vlad.zaharia@epitech.eu>
2023-05-28 10:22:11 +0200

    clear script .js

--- script.js
+++ script.js
@@ -5,19 +5,15 @@
 **Reset game on button or refresh
 */
 
-//Global variables
 const chineseNumbers = ['一','一','二','二','三','三','四','四',
                         '五','五','六','六','七','七','八','八',
                         '九','九','十','十'];
 
 const shuffleCards = shuffleNumbers(chineseNumbers);
 
-//Creates a reference to the bord-container element
 const gameBoard = document.querySelector('#gameContainer');
 const cardContainer = document.querySelector("#cardContainer");
-/***************************************/
 
-//Function to create the gameBorard
 function createBoard(){
     for(let i = 0; i < chineseNumbers.length; i++){
         const flip = document.createElement('div');
@@ -26,19 +22,16 @@ function createBoard(){
         const card = document.createElement('div');
         card.classList.add('card');
         flip.appendChild(card);
-        //Front Card Starts Here
         const front = document.createElement('div');
         front.classList.add('front');
 
         card.appendChild(front);
 
-        //Back Card Starts Here
         const back = document.createElement('div');
         back.classList.add('back');
 
         back.innerHTML = chineseNumbers[i];
         card.appendChild(back);
-        
         cardContainer.appendChild(flip);
     }
 }
@@ -46,7 +39,6 @@ createBoard();
 
 let matchedPairs = 0;
 
-//Function shuffle chineseNumbers
 function shuffleNumbers(arr){
     for(let k = arr.length -1; k > 0; k--){
         const random = Math.floor(Math.random() * (k + 1));
@@ -57,12 +49,11 @@ function shuffleNumbers(arr){
 let cardOne = [];
 let cardTwo = [];
 
-//Function for card flip
 function cardFlip(){
     const flipCard = document.querySelectorAll(".card");
 
     for(let j = 0; j < flipCard.length; j++){
-        flipCard[j].addEventListener("click", function(){ 
+        flipCard[j].addEventListener("click", function() {
             flipCard[j].classList.toggle("cardFlip");
             if (cardOne.length === 0) {
                 cardOne.push(flipCard[j]);
@@ -71,22 +62,18 @@ function cardFlip(){
                 const valueOne = cardOne[0].querySelector('.back').innerHTML;
                 const valueTwo = cardTwo[0].querySelector('.back').innerHTML;
             if (valueOne === valueTwo) {
-                // The cards match
                 cardOne = [];
                 cardTwo = [];
                 matchedPairs++;
                 if (matchedPairs === chineseNumbers.length / 2) {
-                    //Remove hidden class 
                     const alertBox = document.getElementById("alertBox");
                         alertBox.classList.remove("hidden");
-                    //Reload the page on click   
                     const alertButton = document.getElementById('alertButton');
                         alertButton.addEventListener('click', function() {
                         location.reload();
                          })
                     }
                 }else{
-                    // The cards do not match, flip them back over
                      setTimeout(() => {
                         cardOne[0].classList.remove('cardFlip');
                         cardTwo[0].classList.remove('cardFlip');
@@ -100,7 +87,6 @@ function cardFlip(){
 }
 cardFlip();
 
-//Restart the game on click
 restartButton.addEventListener("click", function(){
     cardContainer.innerHTML = "";
     createBoard();
@@ -110,7 +96,6 @@ restartButton.addEventListener("click", function(){
     cardFlip();
 });
 
-//Prefer color theme settings
 const themeSettings = evt => {
     const body = document.body
     const theme = evt.currentTarget.selectedOptions[0].value
